29e94167d6491c0553f1f0f4038f6587c379c2e6
Brian Stolz <brian@tecnobrat.com>
2018-01-16 13:13:55 -0800

    Run in correct shell, use set

--- start.ps1
+++ start.ps1
@@ -3,12 +3,11 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
   $scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
   $arguments = "& '" + $scriptDir + "/windows-networking.ps1'"
-  $p = Start-Process powershell -Verb runAs -ArgumentList $arguments -wait
+  $p = Start-Process powershell -Verb runAs -ArgumentList $arguments -wait -NoNewWindow
   $p.HasExited
   $p.ExitCode
 }
 
-setx TUGBOAT_IP=10.156.156.1
 set TUGBOAT_IP=10.156.156.1
 
 docker-compose stop
